67d134594f302451e91ecd2dcd397807d485c1d0
Tung Huynh <htqtung@gmail.com>
2023-05-22 12:07:59 +0300

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Tung Huynh on 22.5.2023.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

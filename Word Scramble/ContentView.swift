22e7163803565fe7433311f0537a52a4167b0017
Tung Huynh <htqtung@gmail.com>
2023-05-23 15:45:11 +0300

    Optimise game play and UI

--- Word Scramble/ContentView.swift
+++ Word Scramble/ContentView.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var usedWords = [String]()
+    @State private var rootChars = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
     
     @State private var errorTitle = ""
     @State private var errorMessage = ""
     @State private var showingError = false
+    
+    @State private var score = 0
+    let baseScore = 100
     
     func wordError(title: String, message: String) {
         errorTitle = title
@@ -27,11 +31,12 @@ struct ContentView: View {
     }
     
     func isPossible(word: String) -> Bool {
-        var tempWord = rootWord
+        var rootCharsCopy = rootChars
+        let tempAnswer = word.map { String($0) }
         
-        for letter in word {
-            if let pos = tempWord.firstIndex(of: letter) {
-                tempWord.remove(at: pos)
+        for letter in tempAnswer {
+            if let pos = rootCharsCopy.firstIndex(of: letter) {
+                rootCharsCopy.remove(at: pos)
             } else {
                 return false
             }
@@ -55,15 +60,18 @@ struct ContentView: View {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 // 3. Split the string up into an array of strings, splitting on line breaks
                 let allWords = startWords.components(separatedBy: "\n")
-
+                
                 // 4. Pick one random word, or use "silkworm" as a sensible default
                 rootWord = allWords.randomElement() ?? "silkworm"
-
+                rootChars = (rootWord).map { String($0) }
+                
+                rootChars.shuffle()
+                
                 // If we are here everything has worked, so we can exit
                 return
             }
         }
-
+        
         // If were are *here* then there was a problem – trigger a crash and report the error
         fatalError("Could not load start.txt from bundle.")
     }
@@ -71,7 +79,10 @@ struct ContentView: View {
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
-        guard answer.count > 0 else { return }
+        // answer must be at least 3 letters
+        guard answer.count > 2 else {
+            wordError(title: "Too short", message: "Enter a word that's at least 3 letters long")
+            return }
         
         guard isOriginal(word: answer) else {
             wordError(title: "Word used already", message: "Be more original")
@@ -79,7 +90,7 @@ struct ContentView: View {
         }
         
         guard isPossible(word: answer) else {
-            wordError(title: "Word not possible", message: "You can't spell that word from \"\(rootWord)\"")
+            wordError(title: "Word not possible", message: "You can't spell that word from \"\(rootChars)\"")
             return
         }
         
@@ -90,28 +101,52 @@ struct ContentView: View {
         
         withAnimation {
             usedWords.insert(answer, at: 0)
+            if answer != rootWord {
+                score += (answer.count * baseScore)
+            } else {
+                score += 2000
+                wordError(title: "CONGRATULATION!", message: "You found the key word!")
+            }
         }
         newWord = ""
     }
-    
+        
     var body: some View {
         NavigationView {
-            List {
-                Section {
-                    ForEach(usedWords, id: \.self) {word in
-                        HStack {
-                            Image(systemName: "\(word.count).circle")
-                            Text(word)
+            VStack {
+                Text("Score: \(score)").font(.title2)
+                Spacer()
+                HStack {
+                    VStack {
+                        ForEach(rootChars.indices, id: \.self) {index in
+                            Text(rootChars[index].uppercased()).font(.title).bold()
+                        }
+                    }.padding()
+                        .background(Color.yellow, in: RoundedRectangle(cornerRadius: 20))
+                    Spacer()
+                    VStack {
+                        ForEach(usedWords, id: \.self) {word in
+                            HStack {
+                                Image(systemName: "\(word.count).circle")
+                                Text(word)
+                            }
+                            .foregroundColor(word == rootWord ? .blue : .primary)
                         }
                     }
+                    Spacer()
                 }
+                Spacer()
                 
-                Section {
-                    TextField("Enter your word", text: $newWord)
-                        .textInputAutocapitalization(.never)
-                }
+                TextField("Enter your word", text: $newWord)
+                    .textInputAutocapitalization(.never)
+                    .padding()
+                    .background(Color.white, in: RoundedRectangle(cornerRadius: 20))
             }
-            .navigationTitle(rootWord)
+            .padding()
+            .navigationTitle("Word Scramble")
+            .toolbar {
+                Button("New Game", action: startGame).foregroundColor(.yellow)
+            }
             .onSubmit(addNewWord)
             .onAppear(perform: startGame)
             .alert(errorTitle, isPresented: $showingError) {
@@ -119,6 +154,10 @@ struct ContentView: View {
             } message: {
                 Text(errorMessage)
             }
+            .background(LinearGradient(gradient: Gradient(stops: [
+                Gradient.Stop(color: .white, location: 0.80),
+                Gradient.Stop(color: Color.yellow, location: 0.80),
+            ]), startPoint: .top, endPoint: .bottom))
         }
     }
 }
